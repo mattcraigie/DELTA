6e49cd126c70a3bd83429b63e5925dd7b1e98780
mattcraigie <matthewcraigie1@gmail.com>
2024-12-10 15:08:38 +1000

    vmdn_loss fix

--- delta/training/loss_functions.py
+++ delta/training/loss_functions.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn.functional as F
-
+from ..utils.utils import angle_from_trig
 
 def egnn_loss(model, model_input, targets):
     _, _, predictions = model(*model_input)
@@ -8,5 +8,6 @@ def egnn_loss(model, model_input, targets):
 
 
 def vmdn_loss(model, model_inputs, targets):
+    targets = angle_from_trig(targets[:, 0], targets[:, 1])
     return model.loss(*model_inputs, target=targets)
 
