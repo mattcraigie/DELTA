58b8fffb7d44067e87685aafcf980665fef3f0b0
mattcraigie <matthewcraigie1@gmail.com>
2024-12-10 22:01:47 +1000

    mapping adjustments

--- delta/utils/mapping.py
+++ delta/utils/mapping.py
@@ -3,42 +3,66 @@ from .utils import get_model_predictions
 import matplotlib.pyplot as plt
 import argparse
 from .plotting import save_plot
+import numpy as np
 
 
 def load_pretrained_model(model_path):
     model = torch.load(model_path)
     return model
 
-def create_prediction_map(model, dataloader, dataloader_full, device, root_dir, file_name_prefix):
+def create_prediction_map(model, dataloader, dataloader_full, device, root_dir, file_name_prefix, map_type=None, mask_edges=None):
     predictions, targets = get_model_predictions(model, dataloader, device)
     _, targets_full = get_model_predictions(model, dataloader_full, device)
 
     positions = dataloader.dataset.positions
 
+    if mask_edges is None:
+        mask_edges = (520, 540, 20, 40)
+
     mask = (
-        (positions[:, 0] > 500)
-        & (positions[:, 0] < 520)
-        & (positions[:, 1] > 0)
-        & (positions[:, 1] < 20)
+        (positions[:, 0] > mask_edges[0])
+        & (positions[:, 0] < mask_edges[1])
+        & (positions[:, 1] > mask_edges[2])
+        & (positions[:, 1] < mask_edges[3])
     )
 
     positions = positions[mask]
 
+    if map_type is None:
+        map_type = "x_component"
+
+    if map_type == 'x_component':
+        # use the inbuilt map
+        targets, predictions, targets_full = map(lambda x: np.cos(x), [targets, predictions, targets_full])
+        cmap = 'bwr'
+    elif map_type == 'y_component':
+        targets, predictions, targets_full = map(lambda x: np.sin(x), [targets, predictions, targets_full])
+        cmap = 'bwr'
+    elif map_type == 'angle':
+        cmap = 'twilight'
+    else:
+        raise ValueError(f"Map type {map_type} not recognized.")
+
     print(positions.shape)
 
-    fig, axes = plt.subplots(1, 3, figsize=(15, 5))
+    fig, axes = plt.subplots(1, 4, figsize=(20, 5))
 
     # training targets
-    axes[0].scatter(positions[:, 0], positions[:, 1], s=10, c=targets[mask])
+    axes[0].scatter(positions[:, 0], positions[:, 1], s=10, c=targets[mask], cmap=cmap)
     axes[0].set_title("Training Targets")
 
     # model predictions
-    axes[1].scatter(positions[:, 0], positions[:, 1], s=10, c=predictions[mask])
+    axes[1].scatter(positions[:, 0], positions[:, 1], s=10, c=predictions[mask], cmap=cmap)
     axes[1].set_title("Model Predictions")
 
     # true targets
-    axes[2].scatter(positions[:, 0], positions[:, 1], s=10, c=targets_full[mask])
+    axes[2].scatter(positions[:, 0], positions[:, 1], s=10, c=targets_full[mask], cmap=cmap)
     axes[2].set_title("True Targets")
+
+    # prediction error from true targets
+    abs_err = np.abs(predictions[mask] - targets_full[mask])
+    axes[3].scatter(positions[:, 0], positions[:, 1], s=10, c=abs_err, cmap='cool')
+    axes[3].set_title("Prediction Error")
 
     # Save plot
     file_name = file_name_prefix + "_prediction_map.png"
@@ -62,5 +86,6 @@ if __name__ == '__main__':
     model.to(device)
 
     # create prediction map
+    create_prediction_map(model, args.data_path, args.data_full_path, device, args.output_dir, map_type='angle')
 
 
