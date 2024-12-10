ed51be00dcae2846765d5f6f0c65282e414a0b7b
mattcraigie <matthewcraigie1@gmail.com>
2024-12-10 16:09:44 +1000

    angular difference correction

--- delta/training/loss_functions.py
+++ delta/training/loss_functions.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn.functional as F
 from ..utils.utils import angle_from_trig
+import numpy as np
 
 def egnn_loss(model, model_input, targets):
     _, _, predictions = model(*model_input)
@@ -8,6 +9,6 @@ def egnn_loss(model, model_input, targets):
 
 
 def vmdn_loss(model, model_inputs, targets):
-    targets = torch.atan2(targets[:, 1], targets[:, 0]).unsqueeze(-1)
+    targets = torch.atan2(targets[:, 1], targets[:, 0]).unsqueeze(-1) * np.pi
     return model.loss(*model_inputs, target=targets)
 
