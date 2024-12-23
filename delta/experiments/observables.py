526eba082b2d1a3407ccf6e780ca82da5b0a4444
mattcraigie <matthewcraigie1@gmail.com>
2024-12-23 15:11:18 +1000

    observables plotting tweak

--- delta/experiments/observables.py
+++ delta/experiments/observables.py
@@ -322,8 +322,8 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     # load dicts and plot
-    scores_dict_data = np.load(os.path.join(args.output_dir, "scores_dict_data.npy"))
-    scores_dict_full = np.load(os.path.join(args.output_dir, "scores_dict_full.npy"))
+    scores_dict_data = np.load(os.path.join(args.output_dir, "scores_dict_data.npy"), allow_pickle=True)
+    scores_dict_full = np.load(os.path.join(args.output_dir, "scores_dict_full.npy"), allow_pickle=True)
 
     plot_swarm(scores_dict_data, args.output_dir, y_label='% Error', title='Permutation Experiment - Data',
                fname='observables_data.png', threshold=args.threshold)
