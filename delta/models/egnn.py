4e145d2227c8c04ad2905aa94fcaba365b80becd
mattcraigie <matthewcraigie1@gmail.com>
2025-01-19 18:53:23 +1000

    egnn major change for better equivariance

--- delta/models/egnn.py
+++ delta/models/egnn.py
@@ -20,39 +20,58 @@ class EGNN(nn.Module):
         self.edge_mlp = MLP(hidden_dim * 2 + 1, hidden_dim, hidden_layers=[hidden_dim, ])
         self.node_mlp = MLP(hidden_dim * 2, hidden_dim, hidden_layers=[hidden_dim,])
         # self.vector_mlp = MLP(hidden_dim, 2, hidden_layers=[hidden_dim,])
-        self.coord_mlp = MLP(hidden_dim, 1, hidden_layers=[hidden_dim,])
+        self.first_weighting_mlp = MLP(hidden_dim, 1, hidden_layers=[hidden_dim,])
+        self.subsequent_weighting_mlp = MLP(hidden_dim + 3 * hidden_dim, 1, hidden_layers=[hidden_dim,])
 
     def forward(self, h, x, edge_index):
         row, col = edge_index
         h = self.node_embedding(h)
-
-        for _ in range(self.num_layers):
-            rel_pos = x[row] - x[col]  # 3D relative position
-            rel_dist = (rel_pos ** 2).sum(dim=-1, keepdim=True)
-            edge_feat = torch.cat([h[row], h[col], rel_dist], dim=-1)
-            m_ij = self.edge_mlp(edge_feat)
-
-            m_i = scatter(m_ij, row, dim=0, dim_size=h.size(0), reduce='sum')
-
-            node_inp = torch.cat([h, m_i], dim=-1)
-            h = h + self.node_mlp(node_inp)
-
-        # Predicted vector field (2D)
-        # v = self.vector_mlp(h)
+        rel_pos = x[row] - x[col]  # 3D relative position
+        rel_dist = (rel_pos ** 2).sum(dim=-1, keepdim=True)
 
         # Update positions based on relative positions
         rel_theta = torch.arctan2(rel_pos[:, 1], rel_pos[:, 0])
         rel_phi = torch.arctan2(rel_pos[:, 2], torch.sqrt(rel_pos[:, 0] ** 2 + rel_pos[:, 1] ** 2))
-        rel_pos = rel_dist * torch.stack([torch.cos(2 * rel_theta), torch.sin(2 * rel_theta), rel_phi], dim=1)
+        rel_pos_spin2 = rel_dist * torch.stack([torch.cos(2 * rel_theta), torch.sin(2 * rel_theta), rel_phi], dim=1)
 
-        # v = v + scatter(rel_pos * self.coord_mlp(m_ij), row, dim=0,
-        #                 dim_size=x.size(0), reduce='mean')[:, :2]
-        v = scatter(rel_pos * self.coord_mlp(m_ij), row, dim=0,
-                        dim_size=x.size(0), reduce='mean')[:, :2]
+        for layer_num in range(self.num_layers):
+
+            # calculate edge features based on node properties and relative distance to node
+            edge_inputs = torch.cat([h[row], h[col], rel_dist], dim=-1)
+            edge_features_ij = self.edge_mlp(edge_inputs)  # turn these values into hidden_dim edge features
+
+            agg_edge_features_i = scatter(edge_features_ij, row, dim=0, dim_size=h.size(0), reduce='sum')  # sum the edge features for each node
+
+            # update the node values based on the aggregated edge features
+            node_inputs = torch.cat([h, agg_edge_features_i], dim=-1)
+            h = h + self.node_mlp(node_inputs)  # add to existing features rather than overwriting for stability
+
+            '''
+            # Compute latent-size features for relative positions
+            rel_pos_scaled = self.coord_mlp(m_ij).unsqueeze(-1) * rel_pos.unsqueeze(1)  # [num_edges, latent_size, 3]
+
+            # Scatter operation to aggregate the scaled positions
+            v = scatter(rel_pos_scaled, row, dim=0, dim_size=x.size(0), reduce='mean')  # [batch_size, latent_size, 3]
+
+            # Extract the 2D components (x, y) of the vector field
+            v = v[..., :2]  # [batch_size, latent_size, 2]
+            '''
+
+            # calculate the equivariant vectors
+            if layer_num == 0:
+                weighting_inputs = edge_features_ij
+                rel_pos_scaled = rel_pos_spin2.unsqueeze(1) * self.first_weighting_mlp(weighting_inputs).unsqueeze(-1)
+            else:
+                weighting_inputs = torch.cat([edge_features_ij, v.reshape(-1, 3 * hidden_dim)], dim=-1)
+                rel_pos_scaled = rel_pos_spin2.unsqueeze(1) * self.subsequent_weighting_mlp(weighting_inputs).unsqueeze(1)
+
+            v = scatter(rel_pos_scaled, row, dim=0,
+                        dim_size=x.size(0), reduce='mean')  # shape (N, hidden_dim, 3)
+
 
         # Constrain predictions to lie on the unit circle
-        v = F.normalize(v, p=2, dim=-1)
+        v = F.normalize(v[:, :2], p=2, dim=-1)
 
-        return h, x, v
+        return h, x, v  # todo: output only v
 
 
