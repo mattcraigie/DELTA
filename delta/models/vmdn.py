7d64ec62e9e5e2817986fb5d3cbb3b7074886a45
mattcraigie <matthewcraigie1@gmail.com>
2025-01-20 13:07:36 +1000

    isotropy loss re-removed

--- delta/models/vmdn.py
+++ delta/models/vmdn.py
@@ -82,16 +82,16 @@ class VMDN(nn.Module):
             total_loss += self.lambda_kappa * kappa_penalty
 
             # isotropy penalty
-            cos_vals = torch.cos(mu[node_mask])
-            sin_vals = torch.sin(mu[node_mask])
-
-            cos_sum = cos_vals.mean(dim=0)
-            sin_sum = sin_vals.mean(dim=0)
-            isotropy_loss = (cos_sum ** 2 + sin_sum ** 2).mean()
-
-            print(isotropy_loss)
-
-            total_loss += 1.0 * isotropy_loss
+            # cos_vals = torch.cos(mu[node_mask])
+            # sin_vals = torch.sin(mu[node_mask])
+            #
+            # cos_sum = cos_vals.mean(dim=0)
+            # sin_sum = sin_vals.mean(dim=0)
+            # isotropy_loss = (cos_sum ** 2 + sin_sum ** 2).mean()
+            #
+            # print(isotropy_loss)
+            #
+            # total_loss += 1.0 * isotropy_loss
 
         return total_loss
 
